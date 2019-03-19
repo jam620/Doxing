4fded98cc8af764701bdce439c46d325e408f6b6
jam620 <jam620@gmail.com>
2019-03-18 23:12:40 -0500

    Modificado Por tilde
    me dio pereza arreglar el tema de la tilde

--- Doxing.py
+++ Doxing.py
@@ -25,7 +25,7 @@ def menu():
 					  \033[1;31mHacking Live\033[1;m
 			  \033[1;34mHecho por:\033[1;m Victor Bancayan & Kelvin Parra
   				       \033[1;35mVersion: Beta 1.0
-				       \033[1;31m Modificado Dojo Panamá\033[1;m
+				       \033[1;31m Modificado Dojo Panama\033[1;m
   				      
 """
 	print '''
